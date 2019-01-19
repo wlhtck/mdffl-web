ac959b411eef365838d6f86045d21309e0b85087
William Hitchcock <hi@willhitchcock.me>
2019-01-19 16:45:58 -0500

    update test name

--- pages/__tests__/_document.test.jsx
+++ pages/__tests__/_document.test.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Document from '../_document';
 
-describe('pages/index', () => {
+describe('pages/_document', () => {
   it('renders an the Document', () => {
     expect(shallow(<Document />)).toMatchSnapshot();
   });
