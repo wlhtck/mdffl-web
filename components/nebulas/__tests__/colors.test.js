9f4be52e78b509ec3256526e314fac1fcf1afee4
William Hitchcock <hi@willhitchcock.me>
2019-01-19 14:25:55 -0500

    Finalize examples, write tests

---
+++ components/nebulas/__tests__/colors.test.js
@@ -0,0 +1,21 @@
+import {
+  primary, secondary, black, grey, white,
+} from '../colors';
+
+describe('components/nebulas/colors', () => {
+  it('exports primary', () => {
+    expect(primary).toMatchSnapshot();
+  });
+  it('exports secondary', () => {
+    expect(secondary).toMatchSnapshot();
+  });
+  it('exports black', () => {
+    expect(black).toMatchSnapshot();
+  });
+  it('exports grey', () => {
+    expect(grey).toMatchSnapshot();
+  });
+  it('exports white', () => {
+    expect(white).toMatchSnapshot();
+  });
+});
