d68915c00c79885b712a716738b002a4dab6beed
William Hitchcock <hi@willhitchcock.me>
2019-03-08 00:34:24 -0500

    -Add knobs and clean up stories/tests for constellations

--- components/constellations/__spec__/ContentBlock.stories.jsx
+++ components/constellations/__spec__/ContentBlock.stories.jsx
@@ -1,65 +1,98 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { text, boolean, select } from '@storybook/addon-knobs';
 import ContentBlock from '../ContentBlock';
+
+const images = {
+  image1: '//images.ctfassets.net/8wpi5cmhr3lg/2st4Tarcs6i1sDh179cHrT/3dc3fca955cf57417e4195c2a4d09c03/Team_Dynamics.png',
+  image2: '//images.ctfassets.net/8wpi5cmhr3lg/1n0TZGOHyEuCdYH5bXDlSJ/a076437829dacd40410c9599d53b5e96/About_Us.png',
+  image3: '//images.ctfassets.net/8wpi5cmhr3lg/6mW6Tsx5scOYMwocwSskKq/f6222cdd29472f569283df0d4ca0f348/About_Us_Picture.png',
+  image4: '//images.ctfassets.net/8wpi5cmhr3lg/PLDH0id61xTjbGlkiuEKU/7089fadc5e542e1407b79eb11842aa10/34174836_10156493014667146_2406702400535527424_o.jpg',
+};
+
+const imageAlignments = { left: 'left', right: 'right' };
+const textAlignments = { left: 'left', right: 'right', center: 'center' };
 
 storiesOf('ContentBlock', module)
   .add('With image on right', () => (
     <ContentBlock
-      body="
-      We are a LGBT league that’s also open to straight allies. We don’t have a maximum of how many
-      people you can have on your team. You must have at least 7 people present 15 minutes before
-      your scheduled start time in order to play.
-      "
-      headline="About Us"
+      background={boolean('Background', false, 'ContentBlock')}
+      body={text(
+        'body',
+        'We are a LGBT league that’s also open to straight allies. We don’t have a maximum of how many people you can have on your team. You must have at least 7 people present 15 minutes before your scheduled start time in order to play.',
+        'ContentBlock',
+      )}
+      headline={text('headline', 'Team Dynamics', 'ContentBlock')}
       image={{
-        src: '//images.ctfassets.net/8wpi5cmhr3lg/2st4Tarcs6i1sDh179cHrT/3dc3fca955cf57417e4195c2a4d09c03/Team_Dynamics.png',
-        alt: 'Team celebrating victory',
+        src: select(
+          'src',
+          images,
+          '//images.ctfassets.net/8wpi5cmhr3lg/2st4Tarcs6i1sDh179cHrT/3dc3fca955cf57417e4195c2a4d09c03/Team_Dynamics.png',
+          'Image',
+        ),
+        alt: text('alt', 'Team celebrating victory', 'Image'),
       }}
+      imageAlign={select('imageAlign', imageAlignments, 'right', 'ContentBlock')}
+      textAlign={select('textAlign', textAlignments, 'left', 'ContentBlock')}
     />
   ))
   .add('With image on left', () => (
     <ContentBlock
-      background
-      body="
-      The MDFFL is a Metro Detroit based flag football league for LGBT friendly players for
-      recreational and skill development purposes. The league is to promote the growth of the LGBTQ+
-      community and LGBTQ+ friendly players through football. We pursue these goals by providing a
-      venue for the play of football where we encourage players of all levels to participate and
-      improve through practices and pick-up play.
-      "
-      headline="About Us"
+      background={boolean('Background', true, 'ContentBlock')}
+      body={text(
+        'body',
+        'The MDFFL is a Metro Detroit based flag football league for LGBT friendly players for recreational and skill development purposes. The league is to promote the growth of the LGBTQ+ community and LGBTQ+ friendly players through football. We pursue these goals by providing a venue for the play of football where we encourage players of all levels to participate and improve through practices and pick-up play.',
+        'ContentBlock',
+      )}
+      headline={text('headline', 'About Us', 'ContentBlock')}
       image={{
-        src: '//images.ctfassets.net/8wpi5cmhr3lg/1n0TZGOHyEuCdYH5bXDlSJ/a076437829dacd40410c9599d53b5e96/About_Us.png',
-        alt: 'Players walking',
+        src: select(
+          'src',
+          images,
+          '//images.ctfassets.net/8wpi5cmhr3lg/1n0TZGOHyEuCdYH5bXDlSJ/a076437829dacd40410c9599d53b5e96/About_Us.png',
+          'Image',
+        ),
+        alt: text('alt', 'Players walking', 'Image'),
       }}
-      imageAlign="left"
+      imageAlign={select('imageAlign', imageAlignments, 'left', 'ContentBlock')}
+      textAlign={select('textAlign', textAlignments, 'left', 'ContentBlock')}
     />
   ))
   .add('With a cta', () => (
     <ContentBlock
-      background
-      body="We are a LGBT friendly league. It’s a very competitive league so bring your A-game."
-      headline="About Us"
+      background={boolean('Background', true, 'ContentBlock')}
+      body={text(
+        'body',
+        'We are a LGBT friendly league. It’s a very competitive league so bring your A-game.',
+        'ContentBlock',
+      )}
+      headline={text('headline', 'About Us', 'ContentBlock')}
       image={{
-        src: '//images.ctfassets.net/8wpi5cmhr3lg/6mW6Tsx5scOYMwocwSskKq/f6222cdd29472f569283df0d4ca0f348/About_Us_Picture.png',
-        alt: 'Dinosaur mascot on field during warmups',
+        src: select(
+          'src',
+          images,
+          '//images.ctfassets.net/8wpi5cmhr3lg/6mW6Tsx5scOYMwocwSskKq/f6222cdd29472f569283df0d4ca0f348/About_Us_Picture.png',
+          'Image',
+        ),
+        alt: text('alt', 'Dinosaur mascot on field during warmups', 'Image'),
       }}
-      imageAlign="left"
+      imageAlign={select('imageAlign', imageAlignments, 'left', 'ContentBlock')}
+      textAlign={select('textAlign', textAlignments, 'left', 'ContentBlock')}
       cta={{
-        href: 'https://www.mdffl.org/about',
-        children: 'LEARN MORE',
-        external: true,
+        href: text('href', 'https://www.mdffl.org/about', 'CTA'),
+        children: text('children', 'LEARN MORE', 'CTA'),
+        external: boolean('external', true, 'CTA'),
       }}
     />
   ))
   .add('Without image', () => (
     <ContentBlock
-      body="
-      The Metro Detroit Flag Football League (MDFFL) is an LGBT and straight allied flag football
-      league, changing preconceived societal beliefs about sexual orientation and
-      self-identification.
-      "
-      headline="Mission Statement"
-      textAlign="center"
+      body={text(
+        'body',
+        'The Metro Detroit Flag Football League (MDFFL) is an LGBT and straight allied flag football league, changing preconceived societal beliefs about sexual orientation and self-identification.',
+        'ContentBlock',
+      )}
+      headline={text('headline', 'Mission Statement', 'ContentBlock')}
+      textAlign={select('textAlign', textAlignments, 'center', 'ContentBlock')}
     />
   ));
--- components/constellations/__spec__/HeroBlock.stories.jsx
+++ components/constellations/__spec__/HeroBlock.stories.jsx
@@ -1,20 +1,42 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import {
+  text, boolean, select,
+} from '@storybook/addon-knobs';
 import HeroBlock from '../HeroBlock';
 
+const images = {
+  image1: '//images.ctfassets.net/8wpi5cmhr3lg/2st4Tarcs6i1sDh179cHrT/3dc3fca955cf57417e4195c2a4d09c03/Team_Dynamics.png',
+  image2: '//images.ctfassets.net/8wpi5cmhr3lg/1n0TZGOHyEuCdYH5bXDlSJ/a076437829dacd40410c9599d53b5e96/About_Us.png',
+  image3: '//images.ctfassets.net/8wpi5cmhr3lg/6mW6Tsx5scOYMwocwSskKq/f6222cdd29472f569283df0d4ca0f348/About_Us_Picture.png',
+  image4: '//images.ctfassets.net/8wpi5cmhr3lg/PLDH0id61xTjbGlkiuEKU/7089fadc5e542e1407b79eb11842aa10/34174836_10156493014667146_2406702400535527424_o.jpg',
+};
+
 storiesOf('HeroBlock', module)
-  .add('Default', () => (
-    <HeroBlock
-      headline="Ready 2 Ball?"
-      body="
-      Welcome to the Metro Detroit Flag Football League. The 2019 season is upon us. If you want
-      to play with us, click below to register.
-      "
-      imgSrc="//images.ctfassets.net/8wpi5cmhr3lg/PLDH0id61xTjbGlkiuEKU/7089fadc5e542e1407b79eb11842aa10/34174836_10156493014667146_2406702400535527424_o.jpg"
-      cta={{
-        href: '/register',
-        children: 'Google',
-        external: true,
-      }}
-    />
-  ));
+  .add('Default', () => {
+    const props = {
+      headline: text('headline', 'Ready 2 Ball?', 'HeroBlock'),
+      body: text(
+        'body',
+        'Welcome to the Metro Detroit Flag Football League. The 2019 season is upon us. If you want to play with us, click below to register.',
+        'HeroBlock',
+      ),
+      imgSrc: select(
+        'background',
+        images,
+        '//images.ctfassets.net/8wpi5cmhr3lg/PLDH0id61xTjbGlkiuEKU/7089fadc5e542e1407b79eb11842aa10/34174836_10156493014667146_2406702400535527424_o.jpg',
+        'HeroBlock',
+      ),
+      cta: {
+        href: text(
+          'href',
+          'https://www.playyon.com/metro-detroit-flag-football-league/registrations/',
+          'CTA',
+        ),
+        children: text('children', 'Google', 'CTA'),
+        external: boolean('external', true, 'CTA'),
+      },
+    };
+
+    return <HeroBlock {...props} />;
+  });
